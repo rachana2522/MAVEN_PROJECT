6eec949aabc5305c630d61600ac13b3e8b77cc47
Rachana Priyadarshni Samal <CZ0250@cztech.com>
2023-09-17 11:18:00 +0530

    first commit

---
+++ index.sql
@@ -0,0 +1,4 @@
+use first_java_jdbc;
+create table users(id int primary key auto_increment,uname varchar(50),upwd varchar(50),uemail varchar(50),umobile varchar(20));
+desc users;
+select * from users;
